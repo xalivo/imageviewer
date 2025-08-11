d254dc165a81f49541077f6b8a589ea393f13593
xalivo <87896066+xalivo@users.noreply.github.com>
2025-08-11 21:05:45 +0200

    added basic viewer functionality & bash script to rename images

---
+++ rename-images.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+files=(./move-images-here/*)
+for ((i=0; i<${#files[@]}; i++)); do
+    newImgName=$((i+1))
+    mv "${files[$i]}" "./move-images-here/${newImgName}.jpg"
+done
+
+echo "Renamed ${#files[@]} images in ./move-images-here"
